8a3e4ccab77400cbece660867c1cb374cb5ae98a
Osorabrian <osorabrian@gmail.com>
2023-02-13 20:16:13 +0300

    complered assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+      @a = a
+      @b = b
+      @c = c
+  end
+
+  def kind
+    if a == 0 && b == 0 && c == 0
+      raise TriangleError
+    elsif a < 0 || b < 0 || c < 0
+      raise TriangleError
+    elsif (a + b) > c && (b + c) > a && (a + c) > b
+      if @a == @b && @b == @c
+        :equilateral
+      elsif @a == @b || @a == @c || @b == @c
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+  end
+  end
+
+  class TriangleError < StandardError
+  end
 end
